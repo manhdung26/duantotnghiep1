4f9f24eaa9d8d33f54839db6a5d07bf697208a58
manhdung26 <dung098571@gmail.com>
2025-05-17 13:46:44 +0700

    Delete models directory

--- models/modelssuadonhang1.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'config/config.php';
-
-class Customer {
-    public static function all() {
-        $conn = Config::connect();
-        return $conn->query("SELECT id, ten_khach_hang FROM khachhang");
-    }
-}
